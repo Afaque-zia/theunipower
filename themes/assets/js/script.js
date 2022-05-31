250047852f6165b03a8cbaac99aa90c050cbc6a4
Afaque-zia <68955864+Afaque-zia@users.noreply.github.com>
2022-05-31 18:52:04 +0530

    Add files via upload

---
+++ themes/assets/js/script.js
@@ -0,0 +1,31 @@
+
+            // TEXT SELECT
+    var textSelect = 1;
+    $('body').addClass('select1');
+    function selectColor() {
+        textSelect++;
+                if (textSelect > 3) {
+        textSelect = 1;
+                }
+    if (textSelect == 1) {
+        $('body').addClass('select1');
+    $('body').removeClass('select2');
+    $('body').removeClass('select3');
+                }
+    if (textSelect == 2) {
+        $('body').removeClass('select1');
+    $('body').addClass('select2');
+    $('body').removeClass('select3');
+                }
+    if (textSelect == 3) {
+        $('body').removeClass('select1');
+    $('body').removeClass('select2');
+    $('body').addClass('select3');
+                }
+            }
+    $('body').mousedown(function () {
+        selectColor();
+            });
+    $('body').mouseup(function () {
+        selectColor();
+            });
